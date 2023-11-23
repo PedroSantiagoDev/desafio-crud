5efbea100c4e74d328b7808bed62b19ddef2bce9
Pedro Santiago <joaopedrosantiago1103@gmail.com>
2023-11-23 20:25:24 -0300

    feat:implementation of interfaces

---
+++ app/interfaces/ActiveRecordInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace app\interfaces;
+
+interface  ActiveRecordInterface
+{
+    public function execute(ActiveRecordExecuteInterface $activeRecordExecuteInterface);
+
+    public function __set($attribute, $value);
+
+    public function __get($attribute);
+
+    public function getTable();
+
+    public function getAttributes();
+}
