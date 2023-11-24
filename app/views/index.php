e7f61aa27808ba86271692b362e76f47adb94fa2
Pedro Santiago <joaopedrosantiago1103@gmail.com>
2023-11-24 09:48:56 -0300

    test: test calling views

---
+++ app/views/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php $title ?></title>
+</head>
+
+<body>
+    <h2>Task</h2>
+    <div><?php require VIEW_PATH . $controller->view; ?></div>
+</body>
+
+</html>
--- public/index.php
+++ public/index.php
@@ -1,5 +1,31 @@
 <?php
 
-require "../vendor/autoload.php";
+use app\core\AppExtract;
 
-echo "index";
+require "../vendor/autoload.php";
+$whoops = new \Whoops\Run;
+$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+$whoops->register();
+
+$app = new AppExtract;
+$controller = $app->controller();
+$method = $app->method();
+$params = $app->params();
+
+$controller = new $controller;
+
+$controller->$method($params);
+
+if ($_SERVER["REQUEST_METHOD"] === "GET") {
+    if (!isset($controller->data)) {
+        throw new \Exception("A propriedade data é obrigatória");
+    }
+
+    if (!array_key_exists("title", $controller->data)) {
+        throw new \Exception("A propriedade title é obrigatória");
+    }
+
+    extract($controller->data);
+    require "../app/views/index.php";
+}
+
