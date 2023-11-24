f413e1bda8326eb9bd550cc9166b702f5b6d613d
Pedro Santiago <joaopedrosantiago1103@gmail.com>
2023-11-24 15:54:00 -0300

    fix: index.php calling bootstrap

--- public/index.php
+++ public/index.php
@@ -1,31 +1,11 @@
 <?php
 
+require "bootstrap.php";
+
 use app\core\AppExtract;
+use app\core\MyApp;
 
-require "../vendor/autoload.php";
-$whoops = new \Whoops\Run;
-$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
-$whoops->register();
 
-$app = new AppExtract;
-$controller = $app->controller();
-$method = $app->method();
-$params = $app->params();
 
-$controller = new $controller;
 
-$controller->$method($params);
-
-if ($_SERVER["REQUEST_METHOD"] === "GET") {
-    if (!isset($controller->data)) {
-        throw new \Exception("A propriedade data é obrigatória");
-    }
-
-    if (!array_key_exists("title", $controller->data)) {
-        throw new \Exception("A propriedade title é obrigatória");
-    }
-
-    extract($controller->data);
-    require "../app/views/index.php";
-}
 
