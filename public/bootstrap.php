27a2df0bee94aa36ccb962e800353c35340e88bf
Pedro Santiago <joaopedrosantiago1103@gmail.com>
2023-11-25 13:24:34 -0300

    fix: correction to generate the errors

--- public/bootstrap.php
+++ public/bootstrap.php
@@ -6,8 +6,16 @@ use app\core\MyApp;
 require "../vendor/autoload.php";
 $whoops = new \Whoops\Run;
 $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+$whoops->allowQuit(false);
+$whoops->writeToOutput(false);
 $whoops->register();
 
-$myApp = new MyApp(new AppExtract());
-$myApp->controller();
-$myApp->view();
+try {
+    $myApp = new MyApp(new AppExtract());
+    $myApp->controller();
+    $myApp->view();
+} catch (Throwable $e) {
+    $html = $whoops->handleException($e);
+    echo $html;
+}
+
