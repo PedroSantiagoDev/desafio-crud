55759c5e2d0a3af61f85f540bd906cf6b7754b08
Pedro Santiago <joaopedrosantiago1103@gmail.com>
2023-11-26 18:36:28 -0300

    feat: importing the css in index.php which is the main html

--- app/views/index.php
+++ app/views/index.php
@@ -1,15 +1,21 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php $title ?></title>
+    <title><?php echo $title; ?></title>
+
+    <link rel="stylesheet" href="/assets/css/style.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet">
 </head>
 
 <body>
-    <h2>Task</h2>
-    <div><?php require VIEW_PATH . $controller->view; ?></div>
+    <div class="container">
+        <?php require VIEW_PATH . $this->controller->view; ?>
+    </div>
 </body>
 
 </html>
