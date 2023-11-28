8a6385fc2d033f10550436bc75b09c529c247fdb
Pedro Santiago <joaopedrosantiago1103@gmail.com>
2023-11-27 22:31:45 -0300

    feat: Implementing interface with all controller methods

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -2,16 +2,48 @@
 
 namespace app\controllers;
 
-class Home
+use app\interfaces\ControllerInterface;
+use app\models\activerecord\FindAll;
+use app\models\Task;
+
+class Home implements ControllerInterface
 {
     public array $data = [];
     public string $view;
 
-    public function index()
+    public function index(array $args)
     {
+        $tasks = (new Task)->execute(new FindAll(fields: "id, title, description"));
         $this->view = "home.php";
         $this->data = [
-            "title" => "TASK"
+            "title" => "Tasks",
+            "style" => "home-style.css",
+            "tasks" => $tasks
         ];
+    }
+
+    public function edit(array $args)
+    {
+        // TODO: Implement edit() method.
+    }
+
+    public function show(array $args)
+    {
+        // TODO: Implement show() method.
+    }
+
+    public function update(array $args)
+    {
+        // TODO: Implement update() method.
+    }
+
+    public function store()
+    {
+        // TODO: Implement store() method.
+    }
+
+    public function destroy(array $args)
+    {
+        // TODO: Implement destroy() method.
     }
 }
